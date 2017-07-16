6ef0a699628d81fc308a33ed28bc5cbeff447063
winston_hsieh <winston_hsieh@gemteks.com>
2017-07-16 15:23:29 +0800

    Add some custom radio APIs.

--- src/iheartradio-web-api.js
+++ src/iheartradio-web-api.js
@@ -3,6 +3,9 @@
 var HttpManager = require('./http-manager.js'),
     WebApiRequest = require('./webapi-request');
 
+const GET_COMMON_HEADERS = {"Accept": "application/json"};
+const POST_COMMON_HEADERS = {"Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"};
+
 function iHeartRadioWebApi(credentials) {
     this._credentials = credentials || {};
 }
@@ -94,7 +97,7 @@ iHeartRadioWebApi.prototype = {
 
         var request = WebApiRequest.builder()
             .withPath('/api/v1/account/login')
-            .withHeaders({"Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"})
+            .withHeaders(POST_COMMON_HEADERS)
             .withBodyParameters(actualOptions)
             .build();
 
@@ -129,7 +132,449 @@ iHeartRadioWebApi.prototype = {
 
         var request = WebApiRequest.builder()
             .withPath('/api/v1/catalog/getTrackByTrackId')
-            .withHeaders({"Accept": "application/json"})
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+    
+    getArtistByArtistId: function(artistId, includeBio, callback) {
+
+        var actualIncludeBio = false;
+        var actualCallback;
+
+        if (typeof includeBio === 'function') {
+            actualCallback = includeBio;
+        } else {
+            actualIncludeBio = includeBio;
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        actualOptions['artistId'] = artistId;
+        actualOptions['includeBio'] = actualIncludeBio;
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getArtistByArtistId')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getSimilar: function(artistId, callback) {
+        
+        var actualCallback = callback;
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/artist/' + artistId + '/getSimilar')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getTracks: function(artistId, callback) {
+        
+        var actualCallback = callback;
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/artist/' + artistId + '/getTracks')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getAlbumsByCategories: function(categoryId, options, callback) {
+
+        var actualCallback;
+
+        if (typeof options === 'function') {
+            actualCallback = options;
+        } else {
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        actualOptions['categoryId'] = categoryId;
+        if (options.startIndex) {
+            actualOptions['startIndex'] = options.startIndex;
+        }
+        if (options.maxRows) {
+            actualOptions['maxRows'] = options.maxRows;
+        }
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getAlbumsByCategories')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getArtistsByCategories: function(categoryId, options, callback) {
+
+        var actualCallback;
+
+        if (typeof options === 'function') {
+            actualCallback = options;
+        } else {
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        actualOptions['categoryId'] = categoryId;
+        if (options.startIndex) {
+            actualOptions['startIndex'] = options.startIndex;
+        }
+        if (options.maxRows) {
+            actualOptions['maxRows'] = options.maxRows;
+        }
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getArtistsByCategories')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getTracksByCategories: function(categoryId, options, callback) {
+        
+        var actualCallback;
+
+        if (typeof options === 'function') {
+            actualCallback = options;
+        } else {
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        actualOptions['categoryId'] = categoryId;
+        if (options.startIndex) {
+            actualOptions['startIndex'] = options.startIndex;
+        }
+        if (options.maxRows) {
+            actualOptions['maxRows'] = options.maxRows;
+        }
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getTracksByCategories')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getAlbumsByAlbumIds: function(albumId, options, callback) {
+
+        var actualCallback;
+
+        if (typeof options === 'function') {
+            actualCallback = options;
+        } else {
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        actualOptions['albumId'] = albumId;
+        if (options.startIndex) {
+            actualOptions['startIndex'] = options.startIndex;
+        }
+        if (options.maxRows) {
+            actualOptions['maxRows'] = options.maxRows;
+        }
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getAlbumsByAlbumIds')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getTopSongsByGenre: function(genreId, options, callback) {
+
+        var actualCallback;
+
+        if (typeof options === 'function') {
+            actualCallback = options;
+        } else {
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        actualOptions['genreId'] = genreId;
+        if (options.startIndex) {
+            actualOptions['startIndex'] = options.startIndex;
+        }
+        if (options.maxRows) {
+            actualOptions['maxRows'] = options.maxRows;
+        }
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getTopSongsByGenre')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getFeaturedMoodStations: function(callback) {
+
+        var actualCallback;
+
+        if (typeof options === 'function') {
+            actualCallback = options;
+        } else {
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getFeaturedMoodStations')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getFeaturedStationById: function(stationId, callback) {
+
+        var actualCallback;
+
+        if (typeof options === 'function') {
+            actualCallback = options;
+        } else {
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        actualOptions['id'] = stationId;
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getFeaturedStationById')
+            .withHeaders(GET_COMMON_HEADERS)
+            .withQueryParameters(actualOptions)
+            .build();
+
+        var promise = this._performRequest(HttpManager.get, request);
+
+        if (actualCallback) {
+            promise.then(function (data) {
+                actualCallback(null, data);
+            }, function (err) {
+                actualCallback(err);
+            });
+        } else {
+            return promise;
+        }
+    },
+
+    getFeaturedStationBySlug: function(slug, callback) {
+
+        var actualCallback;
+
+        if (typeof options === 'function') {
+            actualCallback = options;
+        } else {
+            actualCallback = callback;
+        }
+
+        var actualOptions = {};
+
+        if (this._credentials) {
+            for (var key in this._credentials) {
+                actualOptions[key] = this._credentials[key];
+            }
+        }
+
+        actualOptions['slug'] = slug;
+
+        var request = WebApiRequest.builder()
+            .withPath('/api/v1/catalog/getFeaturedStationBySlug')
+            .withHeaders(GET_COMMON_HEADERS)
             .withQueryParameters(actualOptions)
             .build();
 
@@ -145,6 +590,7 @@ iHeartRadioWebApi.prototype = {
             return promise;
         }
     }
+
 };
 
 module.exports = iHeartRadioWebApi;
